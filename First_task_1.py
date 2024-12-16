cb06ecf21e51715c5b5f63f6dd98f4562775f712
NewMethod <newmethod.2013@gmail.com>
2024-12-16 14:20:36 +0200

    It` a first homework

---
+++ First_task_1.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import re
+# function for delta days
+def get_days_from_today(date):
+    pattern = r"\w{4}-\w{2}-\w{2}"
+    #test for a right data
+    if re.search(pattern, date) != None:
+        #change str format to datetime format
+        right_date = datetime.strptime(date, "%Y-%m-%d")
+        #generate date today
+        now_date=datetime.today()
+        #take a delta from now and date from date data
+        how_many_days = now_date.toordinal() - right_date.toordinal()
+        return how_many_days
+    else:
+        return "Pleese format date as str YYYY-MM-DD"
+
+print(get_days_from_today('2020-10-09'))
